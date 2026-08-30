7f15e35ee299fc14539b6d8edb2f6466d51b1162
Ayush Teotia <ayushteotia001@gmail.com>
2026-08-30 23:45:20 +0530

    Add DTO classes for event and ticket management

---
+++ backend/src/main/java/com/tickets/domain/dtos/ListTicketResponseDto.java
@@ -0,0 +1,16 @@
+package com.tickets.domain.dtos;
+
+import com.tickets.domain.entities.TicketStatusEnum;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListTicketResponseDto {
+  private UUID id;
+  private TicketStatusEnum status;
+  private ListTicketTicketTypeResponseDto ticketType;
+}
---
+++ backend/src/main/java/com/tickets/domain/dtos/TicketValidationResponseDto.java
@@ -0,0 +1,15 @@
+package com.tickets.domain.dtos;
+
+import com.tickets.domain.entities.TicketValidationStatusEnum;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TicketValidationResponseDto {
+  private UUID ticketId;
+  private TicketValidationStatusEnum status;
+}
